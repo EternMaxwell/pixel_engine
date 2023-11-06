3a35fc19ea05159d69de41ea1103d7f61cf2b3b4
Etern Maxwell <maxwell_dev@outlook.com>
2023-11-06 15:51:18 +0800

    engine test

--- Pixel_engine/src/main/java/com/maxwell_dev/engine/ui/text/TextFrame.java
+++ Pixel_engine/src/main/java/com/maxwell_dev/engine/ui/text/TextFrame.java
@@ -1,11 +1,25 @@
 package com.maxwell_dev.engine.ui.text;
 
+import com.maxwell_dev.engine.render.DrawPipeline;
+import com.maxwell_dev.engine.render.Pipeline;
+import com.maxwell_dev.engine.render.RenderStream;
 import com.maxwell_dev.engine.render.Visible;
+import com.maxwell_dev.globj.Context;
+import com.maxwell_dev.globj.Program;
+import com.maxwell_dev.globj.Shader;
+import com.maxwell_dev.globj.VertexArray;
 
 import java.util.LinkedList;
 import java.util.List;
 
+import static org.lwjgl.opengl.GL46.*;
+
 public class TextFrame{
+
+    public final RenderStream renderStream;
+    private final DrawPipeline pipeline;
+
+
     public static class Vector{
         public Text text;
         public float x;
@@ -19,8 +33,19 @@ public class TextFrame{
 
     private final List<Vector> vectors;
 
-    public TextFrame(){
+    public TextFrame(Context context){
         vectors = new LinkedList<>();
+        Program program = new Program();
+        Shader vertexShader = new Shader(GL_VERTEX_SHADER, "shaders/textShaders/text.vert");
+        Shader fragmentShader = new Shader(GL_FRAGMENT_SHADER, "shaders/textShaders/text.frag");
+        program.vertexShader().attach(vertexShader);
+        program.fragmentShader().attach(fragmentShader);
+        program.linkProgram();
+        pipeline = new DrawPipeline(context);
+        pipeline.program(program);
+        pipeline.vertexArray(new VertexArray());
+
+        renderStream = new RenderStream(pipeline, 1024, 1024, 1024);
     }
 
     public void addText(Text text, float x, float y){
