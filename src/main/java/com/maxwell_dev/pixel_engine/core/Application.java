e3e649f5a59226349ce6467332c41615239cd40d
Etern Maxwell <maxwell_dev@outlook.com>
2024-03-20 22:20:52 +0800

    optimize the application class, a framework in which application is actually just the combination of stages and uiManager, and also doc in Application class

--- src/main/java/com/maxwell_dev/pixel_engine/core/Application.java
+++ src/main/java/com/maxwell_dev/pixel_engine/core/Application.java
@@ -1,24 +1,126 @@
 package com.maxwell_dev.pixel_engine.core;
 
-public abstract class Application {
-    public void start(){
+import com.maxwell_dev.pixel_engine.render.Renderer;
+import com.maxwell_dev.pixel_engine.stage.Stage;
+import com.maxwell_dev.pixel_engine.ui_system.UIManager;
+
+public abstract class Application<T extends Renderer, V extends InputTool> {
+
+    /**
+     * The renderer of the application
+     */
+    public T renderer;
+
+    /**
+     * The stage of the application
+     */
+    public Stage<T, V> stage;
+
+    /**
+     * The UI manager of the application
+     */
+    public UIManager<T, V> uiManager;
+
+    /**
+     * The input tool of the application
+     */
+    public V inputTool;
+
+    /**
+     * The pause value of the application
+     * <p>Only affects the stage</p>
+     * <p>Default value is false</p>
+     */
+    private boolean paused = false;
+
+    /**
+     * Start the application
+     */
+    public void start() {
         init();
-        while(running()){
+        while (running()) {
             loop();
         }
         destroy();
     }
 
+    /**
+     * Initialize the application
+     * <p>Needs to be implemented by the user</p>
+     */
     public abstract void init();
-    public void loop(){
+
+    /**
+     * The main loop of the application
+     * <p>Has automatically implemented by the engine</p>
+     */
+    public void loop() {
         input();
         update();
         render();
     }
-    public abstract void input();
-    public abstract void update();
-    public abstract void render();
+
+    /**
+     * Handle the input
+     * <p>Only uses in the loop method</p>
+     */
+    public void input() {
+        uiManager.input(inputTool);
+        if (!paused && stage != null) {
+            stage.input(inputTool);
+        }
+    }
+
+    /**
+     * Update the application
+     * <p>Only uses in the loop method</p>
+     */
+    public void update() {
+        uiManager.update();
+        if (!paused && stage != null) {
+            stage.update();
+        }
+    }
+
+    /**
+     * Render the application
+     * <p>Mostly uses in the loop method</p>
+     * <p>Also recommend to use in glfwSetSizeCallback</p>
+     */
+    public void render() {
+        if(stage != null){
+            stage.render(renderer);
+        }
+        uiManager.render(renderer);
+    }
+
+    /**
+     * Destroy the application
+     * <p>Needs to be implemented by the user</p>
+     */
     public abstract void destroy();
+
+    /**
+     * Check if the application is running
+     * <p>Needs to be implemented by the user</p>
+     * @return true if the application is running
+     */
     public abstract boolean running();
-    public abstract void pause();
+
+    /**
+     * Check if the application is paused
+     * <p>The pause value only affects the stage</p>
+     * @return true if the application is paused
+     */
+    public boolean paused() {
+        return paused;
+    }
+
+    /**
+     * Pause the application
+     * <p>Only affects the stage</p>
+     */
+    public void pause() {
+        paused = !paused;
+    }
 }
--- src/main/java/com/maxwell_dev/pixel_engine/stage/Stage.java
+++ src/main/java/com/maxwell_dev/pixel_engine/stage/Stage.java
@@ -1,10 +1,11 @@
 package com.maxwell_dev.pixel_engine.stage;
 
+import com.maxwell_dev.pixel_engine.core.InputTool;
 import com.maxwell_dev.pixel_engine.render.Renderer;
 
-public abstract class Stage<T extends Renderer> {
+public abstract class Stage<T extends Renderer, V extends InputTool> {
     public abstract void init();
-    public abstract void input();
+    public abstract void input(InputTool inputTool);
     public abstract void update();
     public abstract void render(T renderer);
     public abstract void dispose();
