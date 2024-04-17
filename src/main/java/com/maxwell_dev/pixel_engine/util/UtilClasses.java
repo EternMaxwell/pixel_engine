982608efb48a99a3d343664facc3feb7025b0ab2
Etern Maxwell <maxwell_dev@outlook.com>
2024-04-17 23:04:59 +0800

    quad tree

---
+++ src/main/java/com/maxwell_dev/pixel_engine/util/UtilClasses.java
@@ -0,0 +1,9 @@
+package com.maxwell_dev.pixel_engine.util;
+
+public class UtilClasses {
+    public interface Float2SetGet{
+        public float x();
+        public float y();
+        public void set(float x, float y);
+    }
+}
--- src/test/java/spaceGameDemo/body/util/GravityQuadTree.java
+++ src/test/java/spaceGameDemo/body/util/GravityQuadTree.java
@@ -1,4 +1,30 @@
 package spaceGameDemo.body.util;
 
 public class GravityQuadTree {
+    private float centerX, centerY;
+    private float radius;
+
+    public GravityQuadTree(float centerX, float centerY, float radius){
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    private class Node{
+        private float mass;
+        private Node parent;
+        private Node leftTop;
+        private Node rightTop;
+        private Node leftBottom;
+        private Node rightBottom;
+        private float centerX;
+        private float centerY;
+        private float radius;
+
+        public Node(float centerX, float centerY, float radius){
+            this.centerX = centerX;
+            this.centerY = centerY;
+            this.radius = radius;
+        }
+    }
 }
